12b4846559cd1d0184031aa76576257f1aeb1433
Francesco Rivola <francesco.rivola@gmail.com>
2020-03-08 15:04:56 +0100

    start and stop agenda in buildApp

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import { connect, getAndSetupDatabase } from './database';
 import { buildServer } from './server';
+import Agenda from 'agenda';
 
 export async function buildApp(options) {
     const dbClient = await connect(options.databaseUrl);
     const db = await getAndSetupDatabase(dbClient, options.databaseName);
+    const agenda = new Agenda({ processEvery: 1000, mongo: db });
+    await agenda.start();
     const server = buildServer();
     return {
         async close() {
             await server.close();
+            await agenda.stop();
             await dbClient.close();
         },
         getServer() {
