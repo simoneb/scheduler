c049c966dc3157f7da5959c99d85d9bf99c3fb9b
Francesco Rivola <francesco.rivola@gmail.com>
2020-04-27 00:31:49 +0200

    added job executions collection indexes

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,7 @@
 import mongodb from 'mongodb';
 
+const ninetyDays = 60 * 60 * 24 * 30 * 3;
+
 export function connect(uri) {
     return mongodb.connect(uri, {
         useNewUrlParser: true,
@@ -10,5 +12,8 @@ export function connect(uri) {
 
 export async function getAndSetupDatabase(client, databaseName) {
     const db = client.db(databaseName);
+    const jobExecutions = db.collection('job-executions');
+    await jobExecutions.createIndex({ createdAt: 1 }, { expireAfterSeconds: ninetyDays });
+    await jobExecutions.createIndex({ jobId:1, createdAt: 1 });
     return db;
 }
